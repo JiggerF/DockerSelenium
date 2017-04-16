8a433739cd2c60988507e2b42df506f4b649a082
Jigger Fantonial <JiggerF@Jiggers-MBP.gateway>
2017-04-16 17:00:32 +1000

    install sh for debian

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,12 +5,24 @@ printf "\nChecking if docker installed\n"
     which docker
 } || {
     printf "\nDocker not present - installing docker\n"
-    curl -sSL https://get.docker.com/ | sh
+    sudo apt-get install docker-ce
     printf "\n Docker version:\n"
     docker version
 }
- 
+
+printf "\nChecking if docker-compose installed\n"
+{
+    # check if docker-compose installed
+    which docker-compose
+} || {
+    printf "\nDocker-compose not present - installing docker-compose\n"
+    sudo apt-get install docker-compose
+    printf "\n Docker-compose version:\n"
+    docker version
+}
+
 usermod -aG docker $(whoami)
  
 printf "\nChecking docker status\n"
 service docker status
+docker-compose up
